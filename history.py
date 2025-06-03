95acf5bf883816bd697b6e7abcaf09bab5025963
npragin <npragin@gmail.com>
2025-06-03 14:51:03 -0700

    Enable CORS.

--- history.py
+++ history.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import base64
 
 app = Flask(__name__)
+CORS(app)
 
 # Configure SQLite database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///history.db"
